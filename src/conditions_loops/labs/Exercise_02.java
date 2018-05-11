dcc32034bfc1683a655e8514de845a0c99621af7
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-05-11 14:41:14 -0700

    Conditions Loops Ex 2 Complete.

--- src/conditions_loops/labs/Exercise_02.java
+++ src/conditions_loops/labs/Exercise_02.java
@@ -13,15 +13,40 @@ import java.util.Scanner;
 public class Exercise_02 {
 
     public static void main(String[] args) {
-
         // create scanner
+        Scanner scanner = new Scanner(System.in);
 
         // prompt user
+        System.out.println("Enter a number between 1 and 7.");
 
-        // assign input to variable as int
+        // assign int to variable
+        int x = scanner.nextInt();
 
-
-        // write completed code here
-
+        // body of code
+        if (x == 1) {
+            System.out.println("Monday");
+        }
+        if (x == 2) {
+            System.out.println("Tuesday");
+        }
+        if (x == 3) {
+            System.out.println("Wednesday");
+        }
+        if (x == 4) {
+            System.out.println("Thursday");
+        }
+        if (x == 5) {
+            System.out.println("Friday");
+        }
+        if (x == 6) {
+            System.out.println("Saturday");
+        }
+        if (x == 7) {
+            System.out.println("Sunday");
+        }
+        if (x >= 8) {
+            System.out.println("other");
+            }
     }
 }
+
