69de3679b30e733f394e685ee0cd8c07b668b2c0
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-05-14 19:04:14 -0700

    Conditions Ex 10 Complete.

--- src/conditions_loops/labs/Exercise_10.java
+++ src/conditions_loops/labs/Exercise_10.java
@@ -8,4 +8,24 @@ package conditions_loops.labs;
  */
 
 public class Exercise_10 {
+    public static void main(String[] args) {
+
+            int c = 1;
+            while ( c <= 10){
+                c++;
+                if ( c == 5) {
+                    continue;
+                }System.out.println("c is " +c);
+            }
+        System.out.println("Program end.");
+        ;
+    }
+
+
+
+
+
+
+
+
 }
