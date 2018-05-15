05fb6b0ae204ddcce63b07782d68047844c15144
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-05-14 18:59:44 -0700

    Conditions Ex 5, need help with sum.

--- src/conditions_loops/labs/Exercise_05.java
+++ src/conditions_loops/labs/Exercise_05.java
@@ -1,5 +1,7 @@
 package conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 5: Calculator
  *
@@ -11,9 +13,47 @@ package conditions_loops.labs;
  *
  *      The sum is: 5050
  *      The average is: 50.5
- *
- *
  */
 
+// How do I correctly find the sum??
+
 public class Exercise_05 {
+    public static void main(String[] args) {
+
+        // scanner
+        Scanner in = new Scanner(System.in);
+
+        // prompt user for input
+        System.out.println("Enter two numbers.");
+
+        // translate user input
+        int x = 0;
+        int y = 0;
+        int sum;
+
+        x = in.nextInt();
+        y = in.nextInt();
+
+        sum = x + y;
+
+ // Option 1
+ /**
+        int z = x + y;
+        float average = z/2;
+
+        // compute the factorial of the numbers through 5
+        int sum = 0;
+        for (int i = z; i <= 100; i++) sum += i;
+
+*/
+ // Option 2
+ /**
+        for (sum; sum <= 100000 ; sum++) {
+            sum +=;
+            float av *= sum;
+        }
+*/
+            System.out.println("The sum is " + sum);
+            System.out.println("The average is ..... FIX ME!!");
+    }
 }
