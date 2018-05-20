ba659da7aefa0c1ad6d901970470afe84d18c858
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-05-20 12:22:54 -0700

    Objects Classes Methods Ex 4 - Complete.

--- src/objects_classes_methods/labs/Exercise_04.java
+++ src/objects_classes_methods/labs/Exercise_04.java
@@ -7,4 +7,42 @@ package objects_classes_methods.labs;
  *
  */
 
+class Overload {
+    void ovlDemo() {
+        System.out.println("no parameters.");
+    }
 
+    void ovlDemo(int a) {
+        System.out.println("one parameter: " +a);
+    }
+
+    int ovlDemo(int a, int b) {
+        System.out.println("two parameters: " +a+ "" +b);
+        return a+b;
+    }
+    double ovlDemo(double a, double b){
+        System.out.println("two double parameters: " +a+ "" +b);
+        return a+b;
+    }
+}
+
+class OverloadDemo{
+    public static void main(String[] args) {
+        Overload ob = new Overload();
+        int resI;
+        double resD;
+
+        ob.ovlDemo();
+        System.out.println();
+
+        ob.ovlDemo(2);
+        System.out.println();
+
+        resI = ob.ovlDemo(4,6);
+        System.out.println();
+
+        resD = ob.ovlDemo(1.1,2.32);
+        System.out.println();
+
+    }
+}
