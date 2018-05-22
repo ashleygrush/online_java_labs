51c16a5363bafe8d1c390ade82c2cc00f0565685
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-05-22 10:22:51 -0700

    Datatypes Operators Ex 6 - Complete.

--- src/datatypes_operators/labs/Exercise_06.java
+++ src/datatypes_operators/labs/Exercise_06.java
@@ -3,7 +3,7 @@ package datatypes_operators.labs;
 /**
  * Data Types and Operators Exercise 6: 2D Array
  *
- *      Creat and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
+ *      Create and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
  *
  *      The output should look something like this:
  *
@@ -15,10 +15,28 @@ package datatypes_operators.labs;
  *
  */
 
-public class Exercise_06 {
+// need to find multiplier??
+
+class Exercise_06 {
 
     public static void main(String[] args) {
-
+// create array (twoDArray)
         int[][] twoDArray = new int[5][5];
+
+        for (int i = 0; i < twoDArray.length; i++) {
+            for (int j = 0; j < twoDArray[i].length; j++) {
+// array formula
+                twoDArray[i][j] = i * (i*3);
+            }
+        }
+// print to screen
+        for (int i = 0; i < twoDArray.length; i++) {
+            for (int j = 0; j < twoDArray[i].length; j++) {
+                System.out.print(twoDArray[i][j] + "  ");
+            }
+            System.out.println();
+        }
+
+
     }
 }
