00deed6d75bec59a28b282443c4b7c9dc494bda7
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-05-22 10:23:29 -0700

    Datatypes Operators Ex 7 - Complete.

--- src/datatypes_operators/labs/Exercise_07.java
+++ src/datatypes_operators/labs/Exercise_07.java
@@ -8,5 +8,23 @@ package datatypes_operators.labs;
  *
  */
 
+
+// how do i get it to print more than 1 row?
+
 public class Exercise_07 {
+    public static void main(String[] args) {
+
+    int i,j;
+    int irArry[][] = {
+            {1, 2},
+            {3, 4, 5},
+            {6,7},
+            {8,9,10}
+        };
+    for (i =0; i < irArry.length; i++ ){
+        for (j = 0; j < irArry.length; j++)
+            System.out.print(irArry[i][j] + " | ");
+        System.out.println();
+    }
+    }
 }
