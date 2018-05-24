f72916fddf3ab6db9fa9b597b4dfda5d240ddec8
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-05-24 14:12:47 -0700

    Datatypes Operators Ex 7 - Complete.

--- src/datatypes_operators/labs/Exercise_07.java
+++ src/datatypes_operators/labs/Exercise_07.java
@@ -14,17 +14,25 @@ package datatypes_operators.labs;
 public class Exercise_07 {
     public static void main(String[] args) {
 
-    int i,j;
-    int irArry[][] = {
-            {1, 2},
-            {3, 4, 5},
-            {6,7},
-            {8,9,10}
+        int i, j;
+        int irArry[][] = {
+                {1, 2},
+                {3, 4, 5},
+                {6, 7},
+                {8, 9, 10}
         };
-    for (i =0; i < irArry.length; i++ ){
-        for (j = 0; j < irArry.length; j++)
-            System.out.print(irArry[i][j] + " | ");
-        System.out.println();
-    }
+//    for (i =0; i < irArry.length; i++ ){
+//        for (j = 0; j < irArry.length; j++)
+//            System.out.print(irArry[i][j] + " | ");
+//        System.out.println();
+//        }
+//    }
+
+        for (int[] arr : irArry) {
+            for (int val : arr) {
+                System.out.print(val);
+            }
+            System.out.println();
+        }
     }
 }
