6386d7f7caeb2a74a99c8306870db9ee77581761
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-05-25 12:00:55 -0700

    Exception Handling Ex 5 - Complete.

--- src/exception_handling/labs/Exercise_05.java
+++ src/exception_handling/labs/Exercise_05.java
@@ -6,3 +6,25 @@ package exception_handling.labs;
  *      Demonstrate how to throw an exception.
  *
  */
+
+class ExceptionEx5 {
+    public static void main(String[] args) {
+        try {
+            throw new ArithmeticException();
+        }
+        catch (ArithmeticException exc) {
+            System.out.println("Caught Arithmetic exception.");
+        }
+        try {
+            int i = ThrowFive();
+        }
+        catch (ArithmeticException exci) {
+            throw exci;
+        }
+    }
+
+    public static int ThrowFive() throws ArithmeticException {
+        throw new ArithmeticException();
+
+    }
+}
