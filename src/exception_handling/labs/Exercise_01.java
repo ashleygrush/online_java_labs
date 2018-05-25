389b50eb3a9cb7216eeaeb80c1e19d8eeafee9d1
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-05-25 11:58:43 -0700

    Exception Handling Ex 1 - Complete.

--- src/exception_handling/labs/Exercise_01.java
+++ src/exception_handling/labs/Exercise_01.java
@@ -5,5 +5,25 @@ package exception_handling.labs;
  *
  *      1) Demonstrate a try/catch.
  *
+ *
+ *
  */
 
+class ExceptionEx1 {
+    static void ExcError() {
+        int array[] = new int[4];
+        array[4] = 6;
+    }
+}
+
+class FixArray {
+    public static void main(String[] args) {
+        // try program
+        try {
+            ExceptionEx1.ExcError();
+        }
+            catch ( ArrayIndexOutOfBoundsException exc) {
+                System.out.println("Out of bounds.");
+            }
+    }
+}
