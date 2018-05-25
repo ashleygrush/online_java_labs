82724a8bf91884ce08db0b8c0613c52eb1fbda63
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-05-25 11:59:11 -0700

    Exception Handling Ex 2 - Complete.

--- src/exception_handling/labs/Exercise_02.java
+++ src/exception_handling/labs/Exercise_02.java
@@ -7,4 +7,29 @@ package exception_handling.labs;
  *
  */
 
+class ExceptionEx2 {
+    public static void main(String[] args) {
+        int array[] = new int[4];
 
+        int i = 0;
+        int x = 3;
+        int y = 0;
+
+        // try program
+        try {
+            array[4] = 5;
+            i = x/y;
+        }
+        // catch the issue
+        catch ( ArithmeticException exc1 ) {
+            System.out.println("Arithmetic exception: " + exc1.toString());
+        }
+        catch ( ArrayIndexOutOfBoundsException exc2 ) {
+            System.out.println("Out of bounds.");
+        }
+        catch ( java.lang.Exception e ) {
+            System.out.println(e.getMessage());
+      }
+
+    }
+}
