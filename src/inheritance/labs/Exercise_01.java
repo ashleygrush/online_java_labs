3a435d4669004129d832fa27c0ecbb6eb9b5aeae
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-05-25 11:57:43 -0700

    Inheritance Ex 1 - Complete.

--- src/inheritance/labs/Exercise_01.java
+++ src/inheritance/labs/Exercise_01.java
@@ -24,3 +24,74 @@ package inheritance.labs;
  *
  */
 
+
+// A class for fish tanks.
+class FishTank {
+    // contains air + water.
+    private int air; // air (0 no air/oxygen needed - 10 needs air/oxygen)
+    private int water; // water gallon size (10,15,30, 60 gal)
+
+    // accessor methods
+    public int getAir() {
+        return air;
+    }
+
+    public void setAir(int air) {
+        this.air = air;
+    }
+
+    public int getWater() {
+        return water;
+    }
+
+    public void setWater(int water) {
+        this.water = water;
+    }
+    void TankRequirements() {
+        System.out.println("This fish tank needs " + air + " percentage of air and " + water + " gallons of water.");
+    }
+
+    // default constructor
+    FishTank( int air, int water ) {
+        air = 0;
+        water = 0;
+    }
+}
+
+// a subclass for fish tank water types
+class FishWater extends FishTank {
+
+    private String style;
+    // contains fresh water + salt water.
+    FishWater(int air, int water) {
+    super(4,30);
+    }
+    int TankSize() {
+        return getAir() + getWater();
+    }
+    void showStyle() {
+        System.out.println("The fish tank is a " + style + " water tank.");
+    }
+}
+
+// a subclass for fish tank chemicals
+class FishHabitat   {
+    public static void main(String[] args) {
+
+
+        // contains habitats fresh water and salt water.
+        FishWater fresh = new FishWater(10, 30);
+        FishWater salt = new FishWater(0, 30);
+
+        System.out.println("info for fresh water tank ");
+        fresh.TankRequirements();
+        fresh.showStyle();
+        System.out.println("This style fish tank needs a percentage of " + fresh.TankSize() + " amount of air.");
+
+        System.out.println("Info for salt water tank ");
+        salt.TankRequirements();
+        salt.showStyle();
+        System.out.println("This style fish tank needs a percentage of " + salt.TankSize() + " amount of air." );
+    }
+}
+
