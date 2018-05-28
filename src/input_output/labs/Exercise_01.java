2b21c1a8c69128b2f436582f34cc8c642f22dff6
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-05-28 16:08:40 -0700

    Input Output Ex 1 - Complete.

--- src/input_output/labs/Exercise_01.java
+++ src/input_output/labs/Exercise_01.java
@@ -1,4 +1,5 @@
 package input_output.labs;
+import java.io.*;
 
 /**
  * Input/Output Exercise 1: File input/output
@@ -9,3 +10,42 @@ package input_output.labs;
  *
  */
 
+class CopyFile {
+    public static void main(String[] args) throws IOException {
+
+        // declare input and output streams
+        BufferedInputStream fin = null;
+        PrintWriter fout = null;
+        FileInputStream fis;
+        int i;
+
+        // copy file
+        try {
+            fis = new FileInputStream("/Users/ashleyalmeida/desktop/FolderExample/file.txt");
+            fin = new BufferedInputStream(fis);
+            fout = new PrintWriter("/Users/ashleyalmeida/desktop/FolderExample/fileCopy.txt");
+                do {
+                    i = fin.read();
+                    if (i != -1) fout.write(i);
+                }
+                while (i != -1);
+        }
+
+        catch (IOException exc) {
+                System.out.println("I/O Error: " + exc);
+        }
+
+        // close connection
+        finally {
+                try {
+                    if (fin != null)
+                        fin.close();
+                    if (fout != null)
+                        fout.close();
+                }
+                catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+        }
+    }
+}
