35e34008b2082c0133700683c0f8c4ac9440cb0d
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-05-28 16:11:02 -0700

    Multi-threading Ex 2 - Complete.

--- src/multi_threading/labs/Exercise_02.java
+++ src/multi_threading/labs/Exercise_02.java
@@ -5,3 +5,31 @@ package multi_threading.labs;
  *
  *      Create an application that creates a Thread using the Thread class
  */
+
+class TrackTime extends Thread {
+
+    public TrackTime (String name){
+        super(name);
+        start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= 5; i++){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(getName() + " " + i);
+        }
+    }
+}
+
+class TrackTimeController {
+    public static void main(String[] args) {
+        TrackTime racer1 = new TrackTime("Ashley lap time - ");
+        TrackTime racer2 = new TrackTime("Max lap time - ");
+        System.out.println("Race Start.");
+    }
+}
