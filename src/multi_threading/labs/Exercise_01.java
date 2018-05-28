1812c1d22dad10475675c983bd0ad1a408281d39
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-05-28 16:10:29 -0700

    Multi-threading Ex 1 - Complete.

--- src/multi_threading/labs/Exercise_01.java
+++ src/multi_threading/labs/Exercise_01.java
@@ -6,4 +6,27 @@ package multi_threading.labs;
  *      Create an application that creates a Thread using the Runnable interface
  */
 
+class TrackRunning implements Runnable{
 
+    Thread runner;
+
+    public TrackRunning (String name){
+        runner = new Thread(this, name);
+        runner.start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 2; i++){
+            System.out.println(runner.getName() + " " + i);
+        }
+    }
+}
+
+class TrackRunningController {
+    public static void main(String[] args) {
+        TrackRunning lap1 = new TrackRunning("Ashley lap - ");
+        TrackRunning lap2 = new TrackRunning("Max lap - ");
+        System.out.println("Run complete.");
+    }
+}
